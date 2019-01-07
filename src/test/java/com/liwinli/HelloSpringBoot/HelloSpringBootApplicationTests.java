b2ba2a62c7f706e91d5aed7f83ff03646bfc5c84
Liwin Lee <LiwinLi@icloud.com>
2019-01-07 11:12:51 +0800

    Add test module

--- src/test/java/com/liwinli/HelloSpringBoot/HelloSpringBootApplicationTests.java
+++ src/test/java/com/liwinli/HelloSpringBoot/HelloSpringBootApplicationTests.java
@@ -1,17 +1,19 @@
 package com.liwinli.HelloSpringBoot;
 
+import com.liwinli.HelloSpringBoot.controller.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;				//必须是static
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HelloSpringBootApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testSayHello() {
+		assertEquals("Hello SB", new HelloController().index());
 	}
-
 }
 

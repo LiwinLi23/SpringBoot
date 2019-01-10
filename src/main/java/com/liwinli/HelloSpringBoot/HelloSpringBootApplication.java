913338f4afac156307436aa68f458f84f52cd470
Liwin Lee <LiwinLi@icloud.com>
2019-01-10 16:51:45 +0800

    Add component scan package.

--- src/main/java/com/liwinli/HelloSpringBoot/HelloSpringBootApplication.java
+++ src/main/java/com/liwinli/HelloSpringBoot/HelloSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.liwinli.HelloSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.liwinli.HelloSpringBoot", "com.xiaoling.eng.user"})
 public class HelloSpringBootApplication {
 
 	public static void main(String[] args) {
